182909be23e82c615b6ea5d6bec0dbf950f3c74f
Kirill <48245126+shalaevk@users.noreply.github.com>
2021-08-20 14:14:47 +0300

    Add analitics

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -68,7 +68,19 @@ const SEO = ({ description, lang, meta, title }) => {
                content: metaDescription,
             },
          ].concat(meta)}
-      />
+      >
+         {/* Global site tag (gtag.js) - Google Analytics  */}
+<script async src={`https://www.googletagmanager.com/gtag/js?id=G-35V17C35SE`}></script>
+<script>
+   {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-35V17C35SE');
+  `}
+</script>
+      </Helmet>
    )
 }
 
